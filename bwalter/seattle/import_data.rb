abb4c0623f5f49f48e92854dffa6aa098d245fd7
Trevor Whitney <twhitney@megasort.mines.edu>
2012-09-20 15:00:53 -0600

    Reorganized project into seattle folder, ignoring large CSV files in data directory

--- bwalter/seattle/import_data.rb
+++ bwalter/seattle/import_data.rb
@@ -8,14 +8,18 @@ end
 db = YAML::load(File.open('db/database.yml'))
 ActiveRecord::Base.establish_connection(db)
 
-lines = File.new('seattle_data.csv').readlines
+lines = File.new('seattle_data_sample.csv').readlines
 header = lines.shift.strip
 keys = header.split(',')
 lines.each do |line|
   params = {}
   values = line.strip.split(',')
   keys.each_with_index do |key, i|
-    params[key] = values[i]
+    if key == "date"
+      params[key] = DateTime.parse(values[i])
+    else
+      params[key] = values[i]
+    end
   end
   SeattleRecord.create(params)
 end
