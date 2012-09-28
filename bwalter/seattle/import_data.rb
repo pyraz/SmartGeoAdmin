4b8c5fce9920d1b274da88e9fbe499576ba8fd7e
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-27 21:35:06 -0600

    Encapsulated work into class

--- bwalter/seattle/import_data.rb
+++ bwalter/seattle/import_data.rb
@@ -9,41 +9,44 @@ end
 class SbSeattleRecord < ActiveRecord::Base
 end
 
-db = YAML::load(File.open('db/database.yml'))
-ActiveRecord::Base.establish_connection(db)
+class Importer
 
-populate_data('data/JCM_NB_tunnel_test.csv', :north)
-populate_Data('data/JCM_SB_tunnel_test.csv', :south)
+  db = YAML::load(File.open('db/database.yml'))
+  ActiveRecord::Base.establish_connection(db)
 
-def self.populate_data(file, direction)
-  lines = File.new(file).readlines
-  header = lines.shift.strip
+  def self.populate_data(file, direction)
+    lines = File.new(file).readlines
+    header = lines.shift.strip
 
-  header.force_encoding("UTF-8").gsub!(
-    "\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+    header.force_encoding("UTF-8").gsub!(
+      "\xEF\xBB\xBF".force_encoding("UTF-8"), '')
 
-  keys = header.split(',')
-  lines.each_with_index do |line, l|
-    params = {}
-    values = line.strip.split(',')
-    keys.each_with_index do |key, i|
-      if key == "date"
-        params[key] = DateTime.parse(values[i])
-      else
-        params[key] = values[i]
+    keys = header.split(',')
+    lines.each_with_index do |line, l|
+      params = {}
+      values = line.strip.split(',')
+      keys.each_with_index do |key, i|
+        if key == "date"
+          params[key] = DateTime.parse(values[i])
+        else
+          params[key] = values[i]
+        end
       end
-    end
-    begin
-      if (direction == :north)
-        NBSeattleRecord.create(params)
-      else
-        SBSeattleRecord.create(params)
+      begin
+        if (direction == :north)
+          NBSeattleRecord.create(params)
+        else
+          SBSeattleRecord.create(params)
+        end
+      rescue ActiveRecord::UnknownAttributeError => e
+        puts "Error on line: #{l}\n#{e.message}"
+        binding.pry
       end
-    rescue ActiveRecord::UnknownAttributeError => e
-      puts "Error on line: #{l}\n#{e.message}"
-      binding.pry
     end
   end
 end
 
+importer = Importer.new
+importer.populate_data('data/JCM_NB_tunnel_test.csv', :north)
+importer.populate_Data('data/JCM_SB_tunnel_test.csv', :south)
 
