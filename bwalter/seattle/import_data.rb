9f11a6e62bc2ef0147b78e9ed0acf6cf0f283a4e
Trevor Whitney <trevorjwhitney@gmail.com>
2012-09-27 21:27:20 -0600

    Removed last DB column, created NB and SB table

--- bwalter/seattle/import_data.rb
+++ bwalter/seattle/import_data.rb
@@ -3,33 +3,47 @@ require 'active_record'
 require 'yaml'
 require 'pry'
 
-class SeattleRecord < ActiveRecord::Base
+class NbSeattleRecord < ActiveRecord::Base
+end
+
+class SbSeattleRecord < ActiveRecord::Base
 end
 
 db = YAML::load(File.open('db/database.yml'))
 ActiveRecord::Base.establish_connection(db)
 
-lines = File.new('data/seattle_data.csv').readlines
-header = lines.shift.strip
+populate_data('data/JCM_NB_tunnel_test.csv', :north)
+populate_Data('data/JCM_SB_tunnel_test.csv', :south)
 
-header.force_encoding("UTF-8").gsub!(
-  "\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+def populate_data(file, direction)
+  lines = File.new(file).readlines
+  header = lines.shift.strip
 
-keys = header.split(',')
-lines.each_with_index do |line, l|
-  params = {}
-  values = line.strip.split(',')
-  keys.each_with_index do |key, i|
-    if key == "date"
-      params[key] = DateTime.parse(values[i])
-    else
-      params[key] = values[i]
+  header.force_encoding("UTF-8").gsub!(
+    "\xEF\xBB\xBF".force_encoding("UTF-8"), '')
+
+  keys = header.split(',')
+  lines.each_with_index do |line, l|
+    params = {}
+    values = line.strip.split(',')
+    keys.each_with_index do |key, i|
+      if key == "date"
+        params[key] = DateTime.parse(values[i])
+      else
+        params[key] = values[i]
+      end
+    end
+    begin
+      if (direction == :north)
+        NBSeattleRecord.create(params)
+      else
+        SBSeattleRecord.create(params)
+      end
+    rescue ActiveRecord::UnknownAttributeError => e
+      puts "Error on line: #{l}\n#{e.message}"
+      binding.pry
     end
   end
-  begin
-    SeattleRecord.create(params)
-  rescue ActiveRecord::UnknownAttributeError => e
-    puts "Error on line: #{l}\n#{e.message}"
-    binding.pry
-  end
 end
+
+
