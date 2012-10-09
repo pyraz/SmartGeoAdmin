645123bcde39b06fbed23690a6b2ae895e693798
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-09 12:21:30 -0600

    Updated FTP code to get files and log stuff

--- bwalter/toronto/ftp.rb
+++ bwalter/toronto/ftp.rb
@@ -1,16 +1,24 @@
 require 'net/ftp'
 require 'timeout'
 require 'pry'
+require 'logger'
+
+log = Logger.new(File.new('ftp.log', 'a'))
+log.level = Logger::INFO
 
 ftp = Net::FTP.new
 ftp.connect('64.40.242.66', '3131')
 ftp.login('admin', 'tysseBDS3')
 ftp.passive = true
 
-puts ftp.pwd
-files = ftp.ls('*.conf')
-puts files
+files = ftp.nlst('/data/depot')
+ftp.chdir('/data/depot')
 
-Timeout.timeout(30) do
-  ftp.gettextfile('niwebserver.conf', 'yoyoyo.conf')
+if files.size == 0
+  log.info("There were no files to download")
+else
+  files.each do |file|
+    log.info("Downloading #{file}...")
+    ftp.getbinaryfile(file, "downloads/#{file}")
+  end
 end
