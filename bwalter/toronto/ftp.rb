abeb67eb2208c233b033cd0c3fa642819014c9f8
Trevor Whitney <trevorjwhitney@gmail.com>
2012-10-10 09:35:14 -0600

    Added error checking to FTP script

--- bwalter/toronto/ftp.rb
+++ bwalter/toronto/ftp.rb
@@ -3,23 +3,36 @@ require 'timeout'
 require 'pry'
 require 'logger'
 
+def connect_ftp
+  ftp = Net::FTP.new
+  ftp.connect('64.40.242.66', '3131')
+  ftp.login('admin', 'tysseBDS3')
+  ftp.passive = true
+  ftp.resume = true
+  return ftp
+end
+
 log = Logger.new(File.new('ftp.log', 'a'))
 log.level = Logger::INFO
 
-ftp = Net::FTP.new
-ftp.connect('64.40.242.66', '3131')
-ftp.login('admin', 'tysseBDS3')
-ftp.passive = true
-
-files = ftp.nlst('/data/depot')
-ftp.chdir('/data/depot')
+@ftp = connect_ftp
+files = @ftp.nlst('/data/depot')
 
 if files.size == 0
   log.info("There were no files to download")
 else
-  files.each do |file|
-    log.info("Downloading #{file}...")
-    ftp.getbinaryfile(file, "downloads/#{file}")
-		ftp.delete(file)
+  log.info("Found #{files.size} files, starting download...")
+  files.each_with_index do |file, index|
+    @ftp.chdir('/data/depot') if @ftp.pwd != '/data/depot' 
+    begin
+      log.info("Downloading file #{index}: #{file}")
+      #@ftp.getbinaryfile(file, "downloads/#{file}")
+  		#@ftp.delete(file)
+    rescue Net::FTPReplyError
+      log.error("FTP connection closed while trying to download #{file}")
+      log.info("Reconnecting to FTP to try again")
+      @ftp.close unless @ftp.closed?
+      @ftp = connect_ftp
+    end
   end
 end
