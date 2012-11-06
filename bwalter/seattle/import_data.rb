8590338ceba8f9a348f1bf0327d4a049de41c2f5
Trevor Whitney <trevorjwhitney@gmail.com>
2012-11-05 22:23:08 -0700

    Moved headers to a seperate file

--- bwalter/seattle/import_data.rb
+++ bwalter/seattle/import_data.rb
@@ -14,9 +14,13 @@ class Importer
   db = YAML::load(File.open('db/database.yml'))
   ActiveRecord::Base.establish_connection(db)
 
-  def populate_data(file, direction)
+  def populate_data(file, direction, options = {})
     lines = File.new(file).readlines
-    header = lines.shift.strip
+    if option[:header] and File.exists?(option[:header])
+      header = read_header(file, options)
+    else
+      header = lines.shift.strip
+    end
 
     header.force_encoding("UTF-8").gsub!(
       "\xEF\xBB\xBF".force_encoding("UTF-8"), '')
@@ -45,8 +49,15 @@ class Importer
     end
   end
 
+  def read_header(lines, options)
+    lines = File.new(options[:header]).readlines
+    header = lines.shift.strip
+  end
+
 end
 
 importer = Importer.new
-importer.populate_data('./JCM_NB_tunnel_test.csv', :north)
-importer.populate_data('./JCM_SB_tunnel_test.csv', :south)
+importer.populate_data('data/JCM_NB_tunnel_nohead.csv', 
+  :north, :header => 'JCM_tunnel_headers.csv')
+importer.populate_data('data/JCM_SB_tunnel_test.csv', 
+  :south, :header => 'JCM_tunnel_headers.csv')
